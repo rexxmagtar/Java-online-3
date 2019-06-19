b965d19fbecb9a072f095b648f72ab1daac6d9ae
rexxmagtar <51932532+rexxmagtar@users.noreply.github.com>
2019-06-19 17:05:57 +0300

    Add files via upload

---
+++ task2_4.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class task2_4 {
+
+
+
+
+    public static void main(String[] args) {
+
+///Проверить на палиндром;
+
+        String str="информатика";
+
+        String slovo=String.valueOf(str.charAt(7))+String.valueOf(str.charAt(3))+
+                String.valueOf(str.charAt(4))+
+                String.valueOf(str.charAt(7));
+
+
+
+
+System.out.print(slovo);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
