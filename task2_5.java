13d9529163bb5d06c7c732d26594d316d51ae1d7
rexxmagtar <51932532+rexxmagtar@users.noreply.github.com>
2019-06-19 17:12:41 +0300

    Add files via upload

---
+++ task2_5.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class task2_5 {
+
+
+
+
+    public static void main(String[] args) {
+
+///Посчитать сколько раз встречается буква a;
+
+        String str="advertismentadvert";
+int count=0;
+for (int i=0;i<str.length();i++){
+
+    if(str.charAt(i)=='a')
+        count++;
+}
+
+System.out.print(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
