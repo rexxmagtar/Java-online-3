b0e3e5ee1416971c62729ca9ab3187e8b36f3acb
rexxmagtar <51932532+rexxmagtar@users.noreply.github.com>
2019-06-19 15:54:44 +0300

    Add files via upload

---
+++ task2_1.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class task2_1 {
+
+
+
+
+    public static void main(String[] args) {
+
+///Колво цифр в строке;
+
+        String str="   1   dsadad     2 3  31 2 ds d  ";
+
+
+
+       int count_max=0;
+       int count=0;
+
+        for (int i=0;i<str.length();i++) {
+            if(str.charAt(i)==' '){
+                count++;
+            }
+            else    {
+                if(count_max<count)
+                    count_max=count;
+
+                count=0;
+            }
+
+        }
+
+System.out.print(count_max);
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
